66125455dfaa195e2220c8ec17bf7c6948b7e64c
Celeromero0808 <115805225+Celeromero0808@users.noreply.github.com>
2023-06-30 11:15:36 -0300

    blacnk space

--- AppB/src/app/app-routing.module.ts
+++ AppB/src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { HistorialsiniComponent } from './pages/historialsini/historialsini.component';
+import { LoginComponent } from './pages/login/login.component';
+import { PersonalComponent } from './pages/personal/personal.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { RegistrodesiniestroComponent } from './pages/registrodesiniestro/registrodesiniestro.component';
+import { TutorialComponent } from './pages/tutorial/tutorial.component';
 
+const routes: Routes = [{path:'Home',component: HomeComponent},
+  {path:'Historialsini',component: HistorialsiniComponent},
+  {path:'Login',component: LoginComponent},
+  {path:'Personal',component: PersonalComponent},
+  {path:'Registro',component: RegistroComponent},
+  {path:'Registrodesiniestro',component:RegistrodesiniestroComponent},
+  {path:'Tutorial',component: TutorialComponent}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- AppB/src/app/app.module.ts
+++ AppB/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module'; //modulo routing
 import { AppComponent } from './app.component';
-import { PagesComponent } from './pages/pages.component';
+import { SharedModule } from './shared/shared.module';
+import { PagesModule } from './pages/pages.module'
+import { ReactiveFormsModule } from '@angular/forms';//importar modulo para formularios reactivos
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PagesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    PagesModule,
+
+    ReactiveFormsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
